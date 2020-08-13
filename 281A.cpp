232dc0e368bc4872a15def3fa7fee313e887cc0b
MajorNick <58568850+MajorNick@users.noreply.github.com>
2020-08-13 19:30:40 +0400

    Add files via upload
    link : https://codeforces.com/problemset/problem/59/A

---
+++ 281A.cpp
@@ -0,0 +1,14 @@
+// link https://codeforces.com/problemset/problem/59/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+
+main (){
+
+	cin>>s;
+
+	if(s[0]>'Z') s[0]-=' ';
+
+	cout<<s;}
