420b9ee78d8a9de4593311dcd1b3a811685cd2c3
MajorNick <58568850+MajorNick@users.noreply.github.com>
2020-08-16 15:37:19 +0400

    Add files via upload
    link : https://codeforces.com/contest/141/problem/A

---
+++ 141A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+main (){
+
+	string s1,s2,s3;
+
+	cin>>s1>>s2>>s3;
+
+	string s=s1+s2;
+
+	sort(s.begin(),s.end());
+
+	sort(s3.begin(),s3.end());
+
+	if(s3==s) cout<<"YES";
+
+	else cout<<"NO";}
