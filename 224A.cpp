df6a02ed523efe9f47fd9d145147ba12d5f1ef17
MajorNick <58568850+MajorNick@users.noreply.github.com>
2020-08-18 13:47:19 +0400

    Add files via upload
    link : https://codeforces.com/problemset/problem/224/A

---
+++ 224A.cpp
@@ -0,0 +1,14 @@
+// link : https://codeforces.com/problemset/problem/224/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+main (){
+
+int a,b,c;
+
+cin>>a>>b>>c;
+
+
+cout<<(sqrt(a*b/c)+sqrt(b*c/a)+sqrt(c*a/b))*4;}
