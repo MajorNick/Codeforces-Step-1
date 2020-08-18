03778569c3a31ffc4216f7f997ee0f7faa36ff52
MajorNick <58568850+MajorNick@users.noreply.github.com>
2020-08-18 21:52:30 +0400

    Add files via upload
    link : https://codeforces.com/problemset/problem/199/A

---
+++ 199A.cpp
@@ -0,0 +1,16 @@
+// link : https://codeforces.com/problemset/problem/199/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a,b,c,d;
+
+int n;
+
+main(){
+
+cin>>n;
+cout<<"0"<<" "<<"0"<<" "<<n;
+
+}
