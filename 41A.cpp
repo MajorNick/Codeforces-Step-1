4722dab2aeba74ef6244920d3edc25437546082f
MajorNick <58568850+MajorNick@users.noreply.github.com>
2020-08-19 14:42:27 +0400

    Add files via upload
    link : https://codeforces.com/problemset/problem/41/A

---
+++ 41A.cpp
@@ -0,0 +1,18 @@
+// link : https://codeforces.com/problemset/problem/41/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string a,b,c;
+
+main (){
+
+cin>>a>>b;
+
+reverse(a.begin(),a.end());
+
+if(a==b) cout<<"YES";
+
+else cout<<"NO";}
+
