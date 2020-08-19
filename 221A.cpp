5f405ca79a1f47a91326369e81dbca7be37087d0
MajorNick <58568850+MajorNick@users.noreply.github.com>
2020-08-19 17:09:16 +0400

    Add files via upload
    link : https://codeforces.com/problemset/problem/221/A

---
+++ 221A.cpp
@@ -0,0 +1,16 @@
+// link : https://codeforces.com/problemset/problem/221/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+main (){
+
+	int n;
+
+	cin>>n;
+
+	cout<<n;
+
+	for(int i=1;i<n;i++)cout<<" "<<i;}
