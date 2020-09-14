c6a60516ce1afd9c8a318b221c7061d8613ccfac
MajorNick <58568850+MajorNick@users.noreply.github.com>
2020-09-14 17:33:16 +0400

    Add files via upload
    link : https://codeforces.com/contest/808/problem/A

---
+++ 808A.cpp
@@ -0,0 +1,30 @@
+// link : https://codeforces.com/contest/808/problem/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a,b,c;
+
+
+
+
+main (){
+
+
+cin>>a;
+int k=a;
+while(a>9){
+
+	a=a/10;
+
+	b++;}
+
+//cout<<a<<endl;
+
+cout<<(a+1)*(int)pow(10,b)-k;}
+
+
+
+
+
